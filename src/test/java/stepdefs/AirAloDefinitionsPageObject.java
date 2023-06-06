65b7e5a8dfc95656bfb23984468c244ea1dd9dcf
Igor Spitkovskyi <Igor.Spitkovskyi@microfocus.com>
2023-06-06 14:19:08 +0300

    different step definition descriptors for generic and page-object-based implementations

--- src/test/java/stepdefs/AirAloDefinitionsPageObject.java
+++ src/test/java/stepdefs/AirAloDefinitionsPageObject.java
@@ -12,24 +12,24 @@ public class AirAloDefinitionsPageObject {
     LandingPage landingPage;
     SearchResults searchResults;
 
-    @Given("^I am on the Airalo homepage$")
+    @Given("^I am on the Airalo homepage object$")
     public void i_can_open_site() throws Exception{
         landingPage = new LandingPage();
         Assert.assertTrue(landingPage.isLandingPageOpened());
     }
 
-    @When("^I search Airalo for \"Canada\"$")
+    @When("^I search Airalo for \"Canada\" option$")
     public void search_for_canada_esims() throws Exception{
         searchResults = landingPage.searchDataPacks("Canada");
 
     }
 
-    @Then("^I should see \"Canada\" listed as a \"Local\" option$")
+    @Then("^I should see \"Canada\" item listed as a \"Local\" option$")
     public void i_can_see_canada_listed_as_local_option() throws Exception{
         searchResults.assertLocalOption("Canada");
     }
 
-    @And("^I close the browser$")
+    @And("^I quit the browser$")
     public void close_browser() throws Exception{
         landingPage.quit();
     }
--- src/test/resources/airalo_search_esim.feature
+++ src/test/resources/airalo_search_esim.feature
@@ -9,3 +9,9 @@ Feature: Searching for a local eSIM
     When I search Airalo for "Canada"
     Then I should see "Canada" listed as a "Local" option
     And I close the browser
+
+  Scenario: eSIM search for Canada using Page Object
+    Given I am on the Airalo homepage object
+    When I search Airalo for "Canada" option
+    Then I should see "Canada" item listed as a "Local" option
+    And I quit the browser
